4dfbf8088e624d3cada49440d42da819ac0ed483
caochitam <caochitam@gmail.com>
2015-08-04 15:50:29 +0700

    x

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+	//      Setup mobile scroll left and right
+    $(".main-container").on("swipeleft",function(){
+	  $(".main-container").css("left","-100%");
+	});
+    $(".main-container").on("swiperight",function(){
+	  $(".main-container").css("left","0");
+	});
+	
 	// Setup menu button
     $('.navicon').click(function(){
         $('.main-container').toggleClass( "turn-left" );
@@ -22,46 +30,7 @@ $(function(){
 		}, 500);
 		return false;
 	});
-//      Setup mobile scroll left and right
-    var current = 0,
-        startX = '',
-        startY = '',
-        endX = '',
-        endY = '',
-        start = '',
-        end = '',
-    /* default */
-    swipeDuration = 1000,
-    swipeDistanceX = 50,
-    swipeDistanceY = 50,
-    thresholdX = 30,
-    thresholdY = 30;
-    $(document).on("touchstart", ".main-container", function (e, ui) {
-        startX = e.originalEvent.touches[0].pageX;
-        startY = e.originalEvent.touches[0].pageY;
-        start = new Date().getTime();
-    }).on("touchmove", ".main-container", function (e, ui) {
-       // e.preventDefault();
-    }).on("touchend", ".main-container", function (e, ui) {
-        endX = e.originalEvent.changedTouches[0].pageX;
-        endY = e.originalEvent.changedTouches[0].pageY;
-        end = new Date().getTime();
-        if ((end - start) < swipeDuration) {
-            if (startX > endX && Math.abs(startY - endY) <= thresholdY && Math.abs(startX - endX) >= swipeDistanceX) {
-                $('.main-container').addClass( "turn-left" );
-                $('.navicon').addClass( "hold" );
-            } else if (startX < endX && Math.abs(startY - endY) <= thresholdY && Math.abs(startX - endX) >= swipeDistanceX) {
-                $('.main-container').removeClass( "turn-left" );
-                $('.navicon').removeClass( "hold" );
-            } else if (startY > endY && Math.abs(startX - endX) <= thresholdX && Math.abs(startY - endY) >= swipeDistanceY) {
-//                 Swipe Up action
-            } else if (startY < endY && Math.abs(startX - endX) <= thresholdX && Math.abs(startY - endY) >= swipeDistanceY) {
-//                 Swipe Down action
-            }
-        }
-    });
-	
+
 	//Show call button
-	if(jQuery.browser.mobile){}
-	else{$('.call-button').hide();}
+	if(typeof jQuery.browser.mobile != 'undefined'){$('.call-button').show()};
 });
