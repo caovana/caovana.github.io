fe2ab85c1797cf78188ab6e3865a4a2c906b6545
caochitam <caochitam@gmail.com>
2015-08-29 14:32:03 +0300

    x

--- assets/js/admin/login.js
+++ assets/js/admin/login.js
@@ -25,7 +25,12 @@ app.controller("loginCtrl", ["$scope", "$rootScope", "$firebaseAuth", function($
 	}
 	$scope.faAuth = function(){
 		$scope.authObj.$authWithOAuthPopup("facebook").then(function(authData) {
-			
+// 		  console.log("Logged in as:", authData.uid);
+// 			$(location).attr('href','index.html');
+// 		  console.log("Logged in as:", authData.uid);
+// 			$(location).attr('href','index.html');
+// 		  console.log("Logged in as:", authData.uid);
+// 			$(location).attr('href','index.html');
 // 		  console.log("Logged in as:", authData.uid);
 // 			$(location).attr('href','index.html');
 		}).catch(function(error) {
