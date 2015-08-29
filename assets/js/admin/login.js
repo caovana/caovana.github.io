5d6da25acd46a67110c904d0438725f91fade0ac
caochitam <caochitam@gmail.com>
2015-08-29 15:23:43 +0700

    x

--- assets/js/admin/login.js
+++ assets/js/admin/login.js
@@ -55,7 +55,7 @@ app.controller("loginCtrl", ["$scope", "$rootScope", "$firebaseAuth", function($
 		});
 	}
 	$scope.faAuth = function(){
-		$firebaseAuth.$authWithOAuthRedirect("google").then(function(authData) {
+		rootRef.$authWithOAuthRedirect("facebook").then(function(authData) {
 		  console.log("Logged in as:", authData.uid);
 		}).catch(function(error) {
 		  console.error("Authentication failed:", error);
