09a83d8329226c772a1b0975f5def2a445910430
caochitam <caochitam@gmail.com>
2015-08-31 06:36:27 +0300

    Admin page

--- admin/dist/js/custom-functions.js
+++ admin/dist/js/custom-functions.js
@@ -3,12 +3,13 @@ var rootRef = new Firebase('https://viemhonghat.firebaseio.com/');
 var authData = rootRef.getAuth();
 function goIndex(){window.location.href = "/admin/pages/index.html";}
 function goLogin(){window.location.href = "/admin/pages/login.html";}
-function checkAuthStatus(){
+function checkAuthStatus(dest){
     if (authData) {
         rootRef.child('users').child(authData.uid).on('value', function(snapshot){
             var data = snapshot.val();
             if(data){
                 if (data.role != 1) goLogin();
+                else if(dest){window.location.href=dest;}
             }else{goLogin();}
         }, function(e){
             console.log(e);
